cf7721dd243ae06562c773a63d7b1a167e330963
Mike <madetunji1376@gmail.com>
2022-06-21 14:06:28 +0100

    javascript codes added  to the script.file

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const mobMenu = document.getElementById("mobile-menu");
 const link = document.querySelector(".one");
 const link2 = document.querySelector(".two ");
 const link3 = document.querySelector(".three");
+const cancel = document.querySelector(".cancel");
 
 menu.addEventListener("click", () => {
   mobMenu.classList.toggle("active");
 });
+cancel.addEventListener("click", () => {
+  mobMenu.classList.remove("active");
+});
+
 link.addEventListener("click", () => {
   mobMenu.classList.remove("active");
 });
