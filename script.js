60fc0921659cae1f565184af576b44aeff650831
milen-ale <milenalebel2@gmail.com>
2022-06-21 22:45:47 -0700

    fix eslint error

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 /*  eslint-env es6  */
-const menu = document.querySelector(".menu");
-const mobMenu = document.getElementById("mobile-menu");
-const link = document.querySelector(".one");
-const link2 = document.querySelector(".two ");
-const link3 = document.querySelector(".three");
-const cancel = document.querySelector(".cancel");
 
-menu.addEventListener("click", () => {
-  mobMenu.classList.toggle("active");
+const menu = document.querySelector('.menu');
+const mobMenu = document.getElementById('mobile-menu');
+const link = document.querySelector('.one');
+const link2 = document.querySelector('.two ');
+const link3 = document.querySelector('.three');
+const cancel = document.querySelector('.cancel');
+
+menu.addEventListener('click', () => {
+  mobMenu.classList.toggle('active');
 });
-cancel.addEventListener("click", () => {
-  mobMenu.classList.remove("active");
+cancel.addEventListener('click', () => {
+  mobMenu.classList.remove('active');
 });
 
-link.addEventListener("click", () => {
-  mobMenu.classList.remove("active");
+link.addEventListener('click', () => {
+  mobMenu.classList.remove('active');
 });
-link2.addEventListener("click", () => {
-  mobMenu.classList.remove("active");
+link2.addEventListener('click', () => {
+  mobMenu.classList.remove('active');
 });
-link3.addEventListener("click", () => {
-  mobMenu.classList.remove("active");
+link3.addEventListener('click', () => {
+  mobMenu.classList.remove('active');
 });
