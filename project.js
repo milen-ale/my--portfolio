d933a003168bc96c058494bbe2ad52a445410d2e
milen-ale <milenalebel2@gmail.com>
2022-06-25 07:38:54 -0700

    fixe somm issues given by the reviewer

--- project.js
+++ project.js
@@ -7,7 +7,7 @@ const projects1 = [
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
     featured_image: './images/card1.png',
-    link_to_live: ['See live', './img/live.png', 'https://github.com'],
+    link_to_live: ['See Source', './img/live.png', 'https://github.com'],
     link_to_source: ['See source', './img/git.png', 'https://github.com'],
     technologies: ['React', 'Redux', 'React-Router'],
     name_detail: ['Milen Alebel ', 'Front-End Dev', '2022'],
@@ -18,7 +18,7 @@ const projects1 = [
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
     featured_image: './images/card2.png',
-    link_to_live: ['See live', './img/live.png', 'https://github.com'],
+    link_to_live: ['See source', './img/live.png', 'https://github.com'],
     link_to_source: ['See source', './img/git.png', 'https://github.com'],
     technologies: ['React', 'Redux', 'React-Router'],
     name_detail: ['Milen Alebel', 'Front-End Dev', '2021'],
@@ -30,7 +30,7 @@ const projects1 = [
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
 
     featured_image: './images/card3.png',
-    link_to_live: ['See live', './img/live.png', 'https://github.com'],
+    link_to_live: ['See source', './img/live.png', 'https://github.com'],
     link_to_source: ['See source', './img/git.png', 'https://github.com'],
     technologies: ['React', 'Redux Toolkit'],
     name_detail: ['Contributors: Milen Alebel', 'Full-stack website', '2022'],
@@ -42,7 +42,7 @@ const projects1 = [
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
 
     featured_image: './images/card4.png',
-    link_to_live: ['See live', './img/live.png', 'https://github.com'],
+    link_to_live: ['See source', './img/live.png', 'https://github.com'],
     link_to_source: ['See source', './img/git.png', 'https://github.com'],
     technologies: ['html', 'css', 'javaScript'],
     name_detail: ['milen alebel', 'Front-End Dev', '2021'],
