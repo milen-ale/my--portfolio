aa08ec16b16930bde793bb65b49cb2963bbcba09
milen-ale <milenalebel2@gmail.com>
2022-06-25 06:59:04 -0700

    fixed all the problems given by my code reviewer

--- project.js
+++ project.js
@@ -1,17 +1,14 @@
-const seeProjectDetail = document.querySelectorAll('.pro-button');
-const projectDetail = document.querySelector('.project-container');
-const mainTag = document.querySelector('.flex-container');
-const header = document.querySelector('.header');
+const projectDetail1 = document.querySelector('.project-container1');
 
-const projects = [
+const projects1 = [
   {
     id: '1',
     name: 'Tonic',
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
     featured_image: './images/card1.png',
-    link_to_live: ['See live', './images/iconlive.png', 'https://github.com'],
-    link_to_source: ['See source', './images/github.png', 'https://github.com'],
+    link_to_live: ['See live', './img/live.png', 'https://github.com'],
+    link_to_source: ['See source', './img/git.png', 'https://github.com'],
     technologies: ['React', 'Redux', 'React-Router'],
     name_detail: ['Milen Alebel ', 'Front-End Dev', '2022'],
   },
@@ -52,69 +49,121 @@ const projects = [
   },
 ];
 
-seeProjectDetail.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    const selectedProject = projects.filter((project) => project.id === e.target.id)[0];
-    projectDetail.classList.remove('hide');
-    mainTag.classList.add('fixed_postion');
-    header.classList.add('fixed_postion');
+const selectedProject = projects1[0];
+const selectedProject1 = projects1[1];
+const selectedProject2 = projects1[2];
+const selectedProject3 = projects1[3];
 
-    projectDetail.innerHTML = `
-    <div class="overlay">
-    <div id="modal">
-    <div>
-   
-    <section class="card  popup-card">
-    <div class="">
-    <div class = popup-header>
-      <h2 class="card-title-two cardtwo">${selectedProject.name}</h2>
-      <img class="close-btn"src="./images/Icon.png"  alt="Icon">
-      </div>
-      <div class="card-about">
-        <p>CANOPY</p>
-        <ul>
-          <li>Back End Dev</li>
-          <li>2015</li>
-        </ul>
-      </div>
-     
-      <div class="card-popup"><img class="card-pop1" src="${selectedProject.featured_image}" alt="" /></div>
-          <div class="flex">
-          <div class="card1-description" >
-          <p class="card1-description-p">
-          ${selectedProject.description}
-          </p>
-        </div>
-        <div>
-      <ul class="language">
-        <li>${selectedProject.technologies[0]}</li>
-        <li>${selectedProject.technologies[1]}</li>
-        <li>${selectedProject.technologies[2]}</li>
+projectDetail1.innerHTML = `
+    <div class="card-container">
+  <div class="card">
+    <img src="${selectedProject.featured_image}" class="cardpic" alt="" />
+   <div class="desktop">
+    <h2 class="card-title">${selectedProject.name}</h2>
+    <h2 class="card-title-two">${selectedProject.name}</h2>
+    <div class="card-about">
+      <p>CANOPY</p>
+      <ul>
+        <li>Back End Dev</li>
+        <li>2015</li>
       </ul>
-
-      <div class="project-detail-buttons">
-      <a  href="${selectedProject.link_to_live[2]}"><button class="work-button_1 see-source">${selectedProject.link_to_live[0]}
-<img class="see-source-img" src=${selectedProject.link_to_live[1]} alt="" /></button>
-</a>
-      <a href="${selectedProject.link_to_source[2]}"><button class="work-button_1 see-source">${selectedProject.link_to_source[0]}
-<img class="see-source-img"  src=${selectedProject.link_to_source[1]}  padding-top=30px ; alt="" /></button>
-</a>
-  </div>
-  </div>
-  </div>
-
+    </div>
+        <p class="card1-description-p">
+        ${selectedProject.description}
+        </p>
     
-  </section>
-</div>
-</div>
-</div>
-  `;
+    <ul class="language">
+      <li>${selectedProject.technologies[0]}</li>
+      <li>${selectedProject.technologies[1]}</li>
+      <li>${selectedProject.technologies[2]}</li>
+    </ul>
+    <button type="button" class="pro-button" id="1">${selectedProject.link_to_live[0]}</button>
 
-    const projectDetailClose = document.querySelector('.close-btn');
-    projectDetailClose.addEventListener('click', () => {
-      mainTag.classList.remove('fixed_postion');
-      header.classList.remove('fixed_postion');
-      projectDetail.classList.add('hide');
-    });
-  });
-});
+</div>
+</div>
+</div>
+
+<div class="card-container">
+  <div class="card">
+    <img src="${selectedProject1.featured_image}" class="cardpic" alt="" />
+   <div class="desktop">
+    <h2 class="card-title">${selectedProject1.name}</h2>
+    <h2 class="card-title-two">${selectedProject1.name}</h2>
+    <div class="card-about">
+      <p>CANOPY</p>
+      <ul>
+        <li>Back End Dev</li>
+        <li>2015</li>
+      </ul>
+    </div>
+        <p class="card1-description-p">
+        ${selectedProject1.description}
+        </p>
+    
+    <ul class="language">
+      <li>${selectedProject1.technologies[0]}</li>
+      <li>${selectedProject1.technologies[1]}</li>
+      <li>${selectedProject1.technologies[2]}</li>
+    </ul>
+    <button type="button" class="pro-button" id="2">${selectedProject.link_to_live[0]}</button>
+
+</div>
+</div>
+</div>
+
+<div class="card-container">
+  <div class="card">
+    <img src="${selectedProject2.featured_image}" class="cardpic" alt="" />
+   <div class="desktop">
+    <h2 class="card-title">${selectedProject2.name}</h2>
+    <h2 class="card-title-two">${selectedProject2.name}</h2>
+    <div class="card-about">
+      <p>CANOPY</p>
+      <ul>
+        <li>Back End Dev</li>
+        <li>2015</li>
+      </ul>
+    </div>
+        <p class="card1-description-p">
+        ${selectedProject2.description}
+        </p>
+    
+    <ul class="language">
+      <li>${selectedProject2.technologies[0]}</li>
+      <li>${selectedProject2.technologies[1]}</li>
+      <li>${selectedProject2.technologies[2]}</li>
+    </ul>
+    <button type="button" class="pro-button" id="3">${selectedProject2.link_to_live[0]}</button>
+
+</div>
+</div>
+</div>
+
+<div class="card-container">
+  <div class="card">
+    <img src="${selectedProject3.featured_image}" class="cardpic" alt="" />
+   <div class="desktop">
+    <h2 class="card-title">${selectedProject3.name}</h2>
+    <h2 class="card-title-two">${selectedProject3.name}</h2>
+    <div class="card-about">
+      <p>CANOPY</p>
+      <ul>
+        <li>Back End Dev</li>
+        <li>2015</li>
+      </ul>
+    </div>
+        <p class="card1-description-p">
+        ${selectedProject3.description}
+        </p>
+    
+    <ul class="language">
+      <li>${selectedProject3.technologies[0]}</li>
+      <li>${selectedProject3.technologies[1]}</li>
+      <li>${selectedProject3.technologies[2]}</li>
+    </ul>
+    <button type="button" class="pro-button" id="4">${selectedProject.link_to_live[0]}</button>
+
+</div>
+</div>
+</div>
+`;
